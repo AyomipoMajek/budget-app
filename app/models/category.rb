8ef2ececcc8789b5f2f851b9bef317fb69a483df
Ayomipo Majek <ayomipomajek@gmail.com>
2023-07-12 22:39:21 +0100

    categories page finished

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,7 @@ class Category < ApplicationRecord
 
   has_many :category_exchanges, class_name: 'CategoryExchange', foreign_key: 'category_id', dependent: :destroy
   has_many :exchanges, through: :category_exchanges
-  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
 
   validates :name, presence: true, length: { maximum: 50 }
 end
